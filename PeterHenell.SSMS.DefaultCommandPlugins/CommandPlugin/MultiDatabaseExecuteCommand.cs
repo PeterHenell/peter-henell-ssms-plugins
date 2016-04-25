f1046f91740f5388c8ec414ba15f91db250c8d24
Peter Henell <mail@peterhenell.se>
2016-04-25 08:50:51 +0200

    adding skeleton for multidatabase query command

---
+++ PeterHenell.SSMS.DefaultCommandPlugins/CommandPlugin/MultiDatabaseExecuteCommand.cs
@@ -0,0 +1,23 @@
+﻿using PeterHenell.SSMS.Plugins.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterHenell.SSMS.DefaultCommandPlugins.CommandPlugin
+{
+    public class MultiDatabaseExecuteCommand : CommandPluginBase
+    {
+        public MultiDatabaseExecuteCommand() :
+            base("MultiDatabaseExecuteCommand", CommandPluginBase.MenuGroups.DataGeneration, "Execute Selected Query On Multiple Databases", "")
+        {
+
+        }
+
+        public override void ExecuteCommand()
+        {
+            
+        }
+    }
+}
